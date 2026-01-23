053569a628fd9e40198217eb2443f4fb9515af26
Maxime Baron <maxime.baron.dev@gmail.com>
2026-01-23 11:50:16 +0100

    Fix logo alt and path

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,19 @@
-"use client";
-
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import { Button } from "@/components/Button";
 
 export function Navbar() {
-  const [open, setOpen] = useState(false);
-
-  const toggleMenu = () => setOpen(!open);
-
   return (
     <header className="max-w-7xl mx-auto">
       <nav className=" navbar mx-8 sm:mx-20 border z-50 border-choco-500 bg-vanilla sticky top-0 flex max-w-7xl items-center justify-between px-6 py-4 rounded-b-2xl">
         <Link href="/">
           <Image
-            src="/images/icon_OMD.svg"
-            alt="Oh My Datte logo"
+            src="/images/icon_omd.svg"
+            alt="Logo de Oh My Datte"
             width={60}
             height={60}
-            className="object-contain"
-          />{" "}
+          />
         </Link>
 
         <div className="hidden md:flex gap-8 font-times items-center text-choco-500">
