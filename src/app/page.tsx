f1008af1492968d0dd302e5f80c7ec1bed34ac94
Maxime Baron <maxime.baron.dev@gmail.com>
2026-01-26 18:18:47 +0100

    Add hero with responsive and animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,10 @@
-import { Button } from "@/components/Button";
-import Image from "next/image";
+import HeroSection from '@/components/HeroSection';
 
 export default function Home() {
   return (
-    <div className="font-sans p-8 gap-16 sm:p-20">
-      <header className="text-choco-500 flex">
-        <div>
-          <h1 className="font-times text-7xl">
-            VOTRE NOUVEAU <br></br>
-            <span className="crunch font-allura text-[164px] relative -top-8">
-              Crunsh
-            </span>
-          </h1>
-          <p>Artisanal ~ Ingrédients de qualité ~ Prêt à déguster</p>
-          <Button>DÉGUSTER</Button>
-        </div>
-
-        <div>
-          <Image
-            src="/images/datte-1.png"
-            alt="Bowl of dates"
-            width={100}
-            height={100}
-            className="object-contain"
-          />
-        </div>
-      </header>
-
-      <main></main>
-    </div>
+    <>
+      <HeroSection/>
+      <div className='h-screen'>t</div>
+    </>
   );
 }
