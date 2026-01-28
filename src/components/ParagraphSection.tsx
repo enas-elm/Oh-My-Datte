60a7cf4853c49fedf251424a45309faa26195d1d
Maxime Baron <maxime.baron.dev@gmail.com>
2026-01-28 17:15:08 +0100

    Add paragraph section

---
+++ src/components/ParagraphSection.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ParagraphSection() {
+  return (
+    <section className='my-28'>
+        <p className=' text-2xl font-archivo text-center w-2/3 mx-auto'>Norem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, dictum est a, mattis tellus. Sed dignissim, metus nec fringilla accumsan, risus sem sollicitudin lacus, ut interdum tellus elit sed risus. Maecenas eget condimentum velit, sit amet feugiat lectus. Class aptent taciti sociosqu ad litora torquent </p>
+    </section>
+  )
+}
