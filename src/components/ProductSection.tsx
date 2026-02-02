f57d7398ea8a497f92c5ed3069b91df4aa551a4b
Maxime Baron <maxime.baron.dev@gmail.com>
2026-02-02 12:07:01 +0100

    Add products

--- src/components/ProductSection.tsx
+++ src/components/ProductSection.tsx
@@ -1,7 +1,20 @@
 import Image from 'next/image';
 import React from 'react';
 
+interface Product {
+    imagePath: string,
+    title: string,
+    subtitle: string,
+    description: string
+}
+
 export default function ProductSection() {
+  const products: Product[] = [
+    {imagePath: "/images/datte-2.png", title: "L'Irrésistible", subtitle: "Blanc · Pistache", description: "Chocolat blanc ivoire et pistaches finement broyées. Douce, élégante — la star qui fait chavirer les cœurs."},
+    {imagePath: "/images/datte-5.png", title: "L'Irrésistible", subtitle: "Blanc · Pistache", description: "Chocolat blanc ivoire et pistaches finement broyées. Douce, élégante — la star qui fait chavirer les cœurs."},
+    {imagePath: "/images/datte-4.png", title: "L'Irrésistible", subtitle: "Blanc · Pistache", description: "Chocolat blanc ivoire et pistaches finement broyées. Douce, élégante — la star qui fait chavirer les cœurs."},
+    {imagePath: "/images/datte-6.png", title: "L'Irrésistible", subtitle: "Blanc · Pistache", description: "Chocolat blanc ivoire et pistaches finement broyées. Douce, élégante — la star qui fait chavirer les cœurs."}
+  ]
 
   return (
     <section className="my-64 container mx-auto px-4 sm:px-6 lg:px-8">
@@ -12,33 +25,14 @@ export default function ProductSection() {
         </h2>
       </div>
       <div className="grid grid-flow-col  grid-cols-1 sm:grid-cols-2 grid-rows-4 sm:grid-rows-2">
-        <div className='flex flex-col justify-center'>
-            <Image src="" alt=""/>
-            <h4 className='uppercase'>Product Title</h4>
-            <p>Product subtitle</p>
-            <p>Product description</p>
-        </div>
-
-        <div className='flex flex-col justify-center'>
-            <Image src="" alt=""/>
-            <h4 className='uppercase'>Product Title</h4>
-            <p>Product subtitle</p>
-            <p>Product description</p>
-        </div>
-
-        <div className='flex flex-col justify-center'>
-            <Image src="" alt=""/>
-            <h4 className='uppercase'>Product Title</h4>
-            <p>Product subtitle</p>
-            <p>Product description</p>
-        </div>
-
-        <div className='flex flex-col justify-center'>
-            <Image src="" alt=""/>
-            <h4 className='uppercase'>Product Title</h4>
-            <p>Product subtitle</p>
-            <p>Product description</p>
-        </div>
+        {products.map((product)=>(
+            <div className='flex flex-col justify-center'>
+                <Image src={product.imagePath} alt={`Datte au ${product.subtitle}`}/>
+                <h4 className='uppercase'>{product.title}</h4>
+                <p>{product.subtitle}</p>
+                <p>{product.description}</p>
+            </div>
+        ))}
       </div>
     </section>
   );
